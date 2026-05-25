eea63ab24bcd1e740a5911cc2e68a4ef83a230c2
Swaraj <swarajanjane0@gmail.com>
2026-05-25 16:21:28 +0530

    basurl update

--- Frontend/src/services/axiosInstance.js
+++ Frontend/src/services/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 let token = localStorage.getItem("token");
 const axiosInstance = axios.create({
   // baseURL: "http://localhost:8080",
-  baseURL: "http://localhost:8080",
+  baseURL: "https://student-data-management-system-rb1m.onrender.com",
   headers: {
     Authorization: `Bearer ${token}`, //local storage ya session storagese tokken share
   },
